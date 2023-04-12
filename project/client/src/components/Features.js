95c6b4db45cd01ea732f08291470c87b4f9e7450
Tanya Malik <106032126+MalikTanya@users.noreply.github.com>
2023-04-12 21:03:48 +0530

    Add files via upload

---
+++ project/client/src/components/Features.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import complaint from "../assets/complaint.png";
+import chat from "../assets/chat.png";
+import laws from "../assets/laws.png";
+import session from "../assets/session.png";
+import counselling from "../assets/counselling.png";
+import track from "../assets/track.png";
+const Features = () => {
+  const featureData = [
+    {
+      img: complaint,
+      title: "File Complain",
+      description: "File your complain with easy process",
+    },
+    {
+      img: chat,
+      title: "Chat",
+      description: "Have one to one conversation with authority",
+    },
+    {
+      img: track,
+      title: "Track Complain",
+      description: "Track your complain status",
+    },
+    {
+      img: laws,
+      title: "Laws",
+      description: "Learn about the laws of harassment",
+    },
+    {
+      img: session,
+      title: "Mentoring Sessions",
+      description: `Information of the session conducted by NGOs`,
+    },
+    {
+      img: counselling,
+      title: "Counselling",
+      description: "Join counselling for any problem without any hesitation",
+    },
+  ];
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+
+  return (
+    <div className="py-6 bg-black">
+      <h2 className="text-white font-bold text-center text-3xl my-6">
+        ASSISTANCE
+      </h2>
+      <div className="grid grid-cols-3 place-items-center gap-6">
+        {featureData.map((features) => {
+          return (
+            <div className="mt-6 flex items-center flex-col cursor-pointer group">
+              <img
+                className="max-w-[120px] group-hover:scale-110 transition-all"
+                src={features.img}
+                alt=""
+                srcset=""
+              />
+              <h2 className="text-white font-bold text-2xl my-2">
+                {features.title}
+              </h2>
+              <p className="text-white font-light mx-4">
+                {features.description}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
