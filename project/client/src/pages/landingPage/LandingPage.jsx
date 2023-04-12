5694c7862c014bb05ede2109a9472be45227c0c1
Tanya Malik <106032126+MalikTanya@users.noreply.github.com>
2023-04-12 21:05:26 +0530

    Update LandingPage.jsx

--- project/client/src/pages/landingPage/LandingPage.jsx
+++ project/client/src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 
 import Hero from "../../components/Hero"
 import Navbar from "../../components/Navbar"
+import Feature from "../../components/Feature"
 
 const LandingPage = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Feature />
     </div>
   )
 }
